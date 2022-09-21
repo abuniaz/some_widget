5e9f467dfdd9502b573e5219188be925acb8395d
Abu Niaz <niaz35-507@diu.edu.bd>
2022-09-21 22:15:04 +0600

    first commit

---
+++ android/app/src/main/kotlin/com/example/multiple_widget/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.multiple_widget
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/popup.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class PupopWidget extends StatefulWidget {
+  const PupopWidget({Key? key}) : super(key: key);
+
+  @override
+  State<PupopWidget> createState() => _PupopWidgetState();
+}
+
+class _PupopWidgetState extends State<PupopWidget> {
+  String title = 'First Page';
+  String firstPage = 'First Page';
+  String secondPage = 'Second Page';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
