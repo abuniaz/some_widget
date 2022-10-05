6a45ac4f93fcd60b206a3c911a3e9ce386133721
Abu Niaz <niaz35-507@diu.edu.bd>
2022-10-05 22:23:05 +0600

    okay

--- lib/slider.dart
+++ lib/slider.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_sliders/sliders.dart';
 
 class SliderPage extends StatefulWidget {
   const SliderPage({Key? key}) : super(key: key);
@@ -8,27 +9,41 @@ class SliderPage extends StatefulWidget {
 }
 
 class _SliderPageState extends State<SliderPage> {
-  double lvalue = 0.0;
-  double uvalue = 200.0;
-  RangeValues values = const RangeValues(0.0, 200.0);
+  // double lvalue = 0.0;
+  // double uvalue = 200.0;
+  // RangeValues values = const RangeValues(0.0, 200.0);
+  double _value = 0.0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-          child: SliderTheme(
-        data: SliderThemeData(),
-        child: RangeSlider(
-            divisions: 20,
-            labels: RangeLabels(values.start.toString(), values.end.toString()),
-            min: lvalue,
-            max: uvalue,
-            values: values,
+        child: SliderTheme(
+          data: SliderTheme.of(context).copyWith(
+            activeTrackColor: Colors.red[700],
+            inactiveTrackColor: Colors.red[100],
+            trackShape: const RectangularSliderTrackShape(),
+            trackHeight: 20.0,
+            thumbColor: Colors.redAccent,
+            thumbShape: const RoundSliderThumbShape(enabledThumbRadius: 12.0),
+            overlayColor: Colors.red.withAlpha(32),
+            overlayShape: const RoundSliderOverlayShape(overlayRadius: 28.0),
+          ),
+          child: SfSlider.vertical(
+            min: 0,
+            max: 200,
+            interval: 20,
+            showTicks: true,
+            showLabels: true,
+            enableTooltip: true,
+            value: _value,
             onChanged: (value) {
               setState(() {
-                values = value;
+                _value = value;
               });
-            }),
-      )),
+            },
+          ),
+        ),
+      ),
     );
   }
 }
