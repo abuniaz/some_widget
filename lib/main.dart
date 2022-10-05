323b84226d3254a8c1f2f9705c608b3d3bc7bd2a
Abu Niaz <niaz35-507@diu.edu.bd>
2022-10-05 11:44:06 +0600

    Niaz

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:multiple_widget/cupertino_menu.dart';
-import 'package:multiple_widget/date_picker.dart';
+
+import 'package:multiple_widget/slider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const CupertinoMenuContext(),
+      home: const SliderPage(),
     );
   }
 }
---
+++ lib/slider.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class SliderPage extends StatefulWidget {
+  const SliderPage({Key? key}) : super(key: key);
+
+  @override
+  State<SliderPage> createState() => _SliderPageState();
+}
+
+class _SliderPageState extends State<SliderPage> {
+  double lvalue = 0.0;
+  double uvalue = 200.0;
+  RangeValues values = const RangeValues(0.0, 200.0);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: RangeSlider(
+              labels:
+                  RangeLabels(values.start.toString(), values.end.toString()),
+              min: lvalue,
+              max: uvalue,
+              values: values,
+              onChanged: (value) {
+                setState(() {
+                  values = value;
+                });
+              })),
+    );
+  }
+}
